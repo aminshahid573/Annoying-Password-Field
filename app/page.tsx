853b131d1a5c4dac16c21a162e45ad47fd15e76e
Shahid Amin <96911219+aminshahid573@users.noreply.github.com>
2025-01-14 10:52:15 +0545

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import AnnoyingPasswordGame from "./components/annoying-password-game"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-24">
+      <AnnoyingPasswordGame />
+    </main>
+  )
+}
+
